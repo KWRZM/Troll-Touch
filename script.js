33fd11fd2993af75538b87e3a80d0eeed1d369bd
KWRZM <84276353+KWRZM@users.noreply.github.com>
2023-05-30 02:42:43 +0100

    +Transition
    Added transition and Anti-Spam system
    also among us audio

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-
 const button = document.querySelector('.glowing-btn');
+const background = document.getElementById('BG');
+const audio = document.getElementById('Audio');
+
+let enAttente = false;
+let max = 5;
+let cpt = 0;
 
 button.addEventListener('mouseover', (e) => {
+
+    // Anti-Spam
+    if(enAttente)return;
+
+
+
     let largeur = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     let hauteur = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
@@ -12,4 +23,27 @@ button.addEventListener('mouseover', (e) => {
     button.style.left = `${newWidth}px`;
 
     console.log(`x= ${button.offsetLeft} y= ${button.offsetTop}`);
+
+
+    if(cpt >= max){
+        background.classList.add("visible");
+        AmongUs();
+        cpt = 0;
+        max = 15;
+    }
+
+    cpt++;
+
+
+    // Timer for Anti-Spam system
+    enAttente = true;
+    timerId = setTimeout(function() {
+        enAttente = false;
+    }, 200);
+
 });
+
+
+function AmongUs(){
+    audio.play();
+}
